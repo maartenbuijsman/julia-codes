871c2bfeab1804d587cff46c442ad39c004d9900
Maarten <maarten.buijsman@usm.edu>
2025-07-30 18:50:48 -0500

    added comment on startup.jl

--- functions/include_functions.jl
+++ functions/include_functions.jl
@@ -2,6 +2,8 @@
 Maarten Buijsman, USM, 2025-7-30
 This file includes all function files.
 pathname is defined in the main file
+NOTE: instead, this file can also be included 
+in ~/.julia/config/startup.jl
 =#
 
 include(string(pathname,"butter_filters.jl"))
