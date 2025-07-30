8b31b28d8288a4d81a1a554c87a0468576a2c373
Maarten Buijsman <w944461@usm.edu>
2025-07-30 14:44:44 -0500

    updated help functionality

--- functions/butter_filters.jl
+++ functions/butter_filters.jl
@@ -5,15 +5,27 @@
 
 using DSP
 
-function bandpass_butter(y,Tl,Th,dt,N)
-#= bandpass filter
-   y: time series that needs to be filtered
-   Tl,Th: low and high cutoff times 
-   dt: delta-t
-   N: order of the filter
-   Make sure all the time varibles have the same time units   
-=# 
+"""
+bandpass_butter(y,Tl,Th,dt,N)
 
+bandpass butterworth filter, returns filtered time series
+
+# Arguments
+y: time series that needs to be filtered.
+
+Tl, Th: low and high cutoff periods. 
+
+dt: delta-t.
+
+N: order of the filter.
+
+Make sure all the time variables have the same time units!   
+
+# Example: 9-15 hour bandpass filter for 1 cph sampling (Δt=1hour)
+yf = bandpass_butter(y,9,15,1,4)
+
+"""
+function bandpass_butter(y,Tl,Th,dt,N)
     f1=1/Th  
     f2=1/Tl 
     fs=1/dt
