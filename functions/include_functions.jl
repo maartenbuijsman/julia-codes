0319e3bdd5c4336098617429c035aa602f8b2858
Maarten <maarten.buijsman@usm.edu>
2025-07-30 18:29:55 -0500

    added comments

--- functions/include_functions.jl
+++ functions/include_functions.jl
@@ -1,1 +1,7 @@
+#= include_functions.jl
+Maarten Buijsman, USM, 2025-7-30
+This file includes all function files.
+pathname is defined in the main file
+=#
+
 include(string(pathname,"butter_filters.jl"))
