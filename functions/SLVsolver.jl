0b802c34d00cf0b8982b65833b97379a1e303835
Maarten <maarten.buijsman@usm.edu>
2025-08-01 14:14:12 -0500

    created new solver function

---
+++ functions/SLVsolver.jl
@@ -0,0 +1,36 @@
+#= SLVsolver.jl
+   Maarten Buijsman, USM, 2025-7-30
+   This function solves the Sturm-Liouville equation 
+   for non-equidistant z co0rdinates 
+=#
+
+using LinearAlgebra
+
+"""
+SLVsolver(y,Tl,Th,dt,N)
+
+bandpass butterworth filter, returns filtered time series
+
+# Arguments
+y: time series that needs to be filtered.
+
+Tl, Th: low and high cutoff periods. 
+
+dt: delta-t.
+
+N: order of the filter.
+
+Make sure all the time variables have the same time units!   
+
+# Example: 9-15 hour bandpass filter for 1 cph sampling (Δt=1hour)
+yf = bandpass_butter(y,9,15,1,4)
+
+"""
+function bandpass_butter(y,Tl,Th,dt,N)
+    f1=1/Th  
+    f2=1/Tl 
+    fs=1/dt
+    b = digitalfilter(Bandpass(f1, f2), Butterworth(N); fs=fs)
+
+    return filtfilt(b, y)
+end
